086487e748b586e6f6df9a9ab29d76f7e92ab142
tyde81 <pedrohfonseca.1981@gmail.com>
2024-06-01 02:38:44 -0300

    feat(deployments): render deployments in frontend

--- ignite_server/config/dev.exs
+++ ignite_server/config/dev.exs
@@ -10,6 +10,9 @@ config :ignite_server, Ignite.Server.Repo,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
+config :ignite,
+  app_name: "Ignite Dashboard"
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
--- ignite_server/lib/ignite_server_web/live/user_home_live.ex
+++ ignite_server/lib/ignite_server_web/live/user_home_live.ex
@@ -1,21 +1,45 @@
 defmodule Ignite.ServerWeb.UserHomeLive do
+  alias Ignite.Server.Deployments
   use Ignite.ServerWeb, :live_view
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Sign in to account
-      </.header>
-
+    <div class="">
+      <%!-- <.header class="text-center">
+        Deployments
+      </.header> --%>
       <div>
-
+        <%= for deployment <- Deployments.list_deployments() do %>
+          <div class="shadow-sm p-4 my-2 rounded border-l-4 border-red-700">
+              <div>
+                <div class="flex justify-between">
+                  <p><%= deployment.name %></p>
+                  <.button phx-click="run_deployment" phx-value-id={deployment.id}>
+                    Run
+                    <.icon name="hero-chevron-right-mini" class="mt-0.5 h-5 w-5 flex-none" />
+                  </.button>
+                </div>
+                <div>
+                  <span><%= deployment.inserted_at %></span>
+                </div>
+              </div>
+          </div>
+        <% end %>
       </div>
     </div>
     """
   end
 
+  def handle_event("run_deployment", %{"id" => id}, socket) do
+    # TO DO
+
+    {:noreply, socket}
+  end
+
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, page_title: "e")}
+    app_name = Application.fetch_env!(:ignite, :app_name)
+
+    {:ok, assign(socket, page_title: app_name)}
   end
 end
